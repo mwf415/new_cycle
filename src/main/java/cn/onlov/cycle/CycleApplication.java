cd99c1c8737a7b506db6d0cdc20a30bfdfd4bb7a
mwf415 <mwf415@126.com>
2019-01-05 11:09:02 +0800

    初始化提交

--- src/main/java/cn/onlov/cycle/CycleApplication.java
+++ src/main/java/cn/onlov/cycle/CycleApplication.java
@@ -1,9 +1,15 @@
 package cn.onlov.cycle;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan(basePackages = "cn.onlov.cycle.core.dao.mapper")
+@EnableTransactionManagement
+@EnableScheduling
 public class CycleApplication {
 
     public static void main(String[] args) {
